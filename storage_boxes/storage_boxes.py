08ee1b3908cf2c34a29f4c88190b101dda7c8941
Jakob Helmecke <jakob.helmecke@yeebase.com>
2021-11-30 22:37:38 +0100

    fix(storage_boxes): use variables

--- storage_boxes/storage_boxes.py
+++ storage_boxes/storage_boxes.py
@@ -6,38 +6,30 @@ if "show_object" not in globals():
     def show_object(*args, **kwargs):
         pass
 
+
 debug = {"alpha": 0.5}
 
 box_size = (3, 1)
 box_length = 54.8
 box_width = 54.8
 box_height = 75
+base_height = 4
 
 
 def createBottom(self):
-    s1 = (
-        cq.Sketch()
-        .rect(45.8, 45.8)
-        .vertices()
-        .fillet(3)
-    )
+    s1 = cq.Sketch().rect(box_width - 9, box_length - 9).vertices().fillet(3)
 
-    s2 = (
-        cq.Sketch()
-        .rect(53.8, 53.8)
-        .vertices()
-        .fillet(3)
-    )
+    s2 = cq.Sketch().rect(box_width - 1, box_length - 1).vertices().fillet(3)
 
     R = (
         cq.Workplane()
         .placeSketch(
-            s1.moved(cq.Location(cq.Vector(54.8 / 2, 54.8 / 2, 0))),
-            s2.moved(cq.Location(cq.Vector(54.8 / 2, 54.8 / 2, 4)))
+            s1.moved(cq.Location(cq.Vector(box_length / 2, box_width / 2, 0))),
+            s2.moved(
+                cq.Location(cq.Vector(box_length / 2, box_width / 2, base_height))
+            ),
         )
         .loft()
-        # .faces(">Z")
-        # .shell(-4 * 0.28)
     )
 
     return self.eachpoint(lambda loc: R.val().located(loc), True)
@@ -46,17 +38,23 @@ def createBottom(self):
 cq.Workplane.createBottom = createBottom
 
 
-def createMiddle(self, size=(1, 1)):
+def createMiddle(self):
     s1 = (
         cq.Sketch()
-        .rect(53.8 * box_size[1], 53.8 * box_size[0] + (-1 + 1 * box_size[0]))
+        .rect(
+            (box_width - 1) * box_size[1] + (-1 + 1 * box_size[1]),
+            (box_length - 1) * box_size[0] + (-1 + 1 * box_size[0]),
+        )
         .vertices()
         .fillet(3)
     )
 
     s2 = (
         cq.Sketch()
-        .rect(54.8 * box_size[1], 54.8 * box_size[0] + (-1 + 1 * box_size[0]))
+        .rect(
+            box_width * box_size[1] + (-1 + 1 * box_size[1]),
+            box_length * box_size[0] + (-1 + 1 * box_size[0]),
+        )
         .vertices()
         .fillet(3)
     )
@@ -64,12 +62,24 @@ def createMiddle(self, size=(1, 1)):
     R = (
         cq.Workplane()
         .placeSketch(
-            s1.moved(cq.Location(cq.Vector(
-                54.8 / 2 * box_size[0], 54.8 / 2 * box_size[1], 4))
+            s1.moved(
+                cq.Location(
+                    cq.Vector(
+                        box_length / 2 * box_size[0],
+                        box_width / 2 * box_size[1],
+                        base_height,
+                    )
+                )
             ),
-            s2.moved(cq.Location(cq.Vector(
-                54.8 / 2 * box_size[0], 54.8 / 2 * box_size[1], box_height - 4))
-            )
+            s2.moved(
+                cq.Location(
+                    cq.Vector(
+                        box_length / 2 * box_size[0],
+                        box_width / 2 * box_size[1],
+                        box_height - base_height,
+                    )
+                )
+            ),
         )
         .loft()
     )
@@ -88,7 +98,7 @@ res = (
     cq.Workplane()
     .pushPoints(pts)
     .createBottom()
-    .union(cq.Workplane().createMiddle(size=box_size))
+    .union(cq.Workplane().createMiddle())
     .faces(">Z")
     .shell(-4 * 0.28)
     .faces("<Z[2]")
